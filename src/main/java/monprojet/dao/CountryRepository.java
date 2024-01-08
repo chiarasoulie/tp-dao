e8063f7b83425bc9389f9bca61fd7b411c2fee70
csoul00 <souliechiara@gmail.com>
2024-01-08 15:47:38 +0100

    fin tp

--- src/main/java/monprojet/dao/CountryRepository.java
+++ src/main/java/monprojet/dao/CountryRepository.java
@@ -11,5 +11,15 @@ import monprojet.entity.Country;
 // This will be AUTO IMPLEMENTED by Spring 
 
 public interface CountryRepository extends JpaRepository<Country, Integer> {
+    @Query ("SELECT SUM(c.population) AS population "
+    + "FROM City c "
+    + "WHERE c.country.id = :id ")
+    public int sommePop (int id) ;
+
+    @Query ("Select c.country.name as nom , sum (c.population) as population "
+            + "FROM City c "
+    + "GROUP BY name ")
+    public List<Projection> lesPays () ;
 
 }
+
